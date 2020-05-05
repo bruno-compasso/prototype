e73efa95961d198a1cb695c63eb1633bedae215a
bruno pereira <bruno.pereira@compasso.com.br>
2020-05-05 17:21:28 -0300

    ...

--- pages/lessons/react/01.js
+++ pages/lessons/react/01.js
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 import Layout from '../../../components/layout/layout'
 import ReactMarkdown from 'react-markdown'
-import codeRenderer from './code-renderer'
-import sampleCode from './code'
+import codeRenderer from '../../../lessons/react/code-renderer'
+import sampleCode from '../../../lessons/react/code'
 
 export default function _01() {
   return (
