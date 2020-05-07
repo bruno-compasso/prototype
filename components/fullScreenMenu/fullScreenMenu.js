c3728e8ffc072f400490381fd43fc78ccac1b5a1
bruno pereira <bruno.pereira@compasso.com.br>
2020-05-07 09:37:08 -0300

    ...

--- components/fullScreenMenu/fullScreenMenu.js
+++ components/fullScreenMenu/fullScreenMenu.js
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import styles from './FullScreenMenu.module.css'
+import styles from './fullScreenMenu.module.css'
 
 class FullScreenMenu extends React.Component {
   render() {
