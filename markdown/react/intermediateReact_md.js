c7717c1ce85709d399764e457aa047b9a6351675
bruno pereira <bruno.pereira@compasso.com.br>
2020-05-19 15:35:53 -0300

    ...

---
+++ markdown/react/intermediateReact_md.js
@@ -0,0 +1,3 @@
+export default `
+Coming soon...
+`
---
+++ pages/tutorials/react/react-basics.js
@@ -0,0 +1,15 @@
+import MdLesson from '../../../components/MdLesson/MdLesson'
+import reactBasics_md from '../../../markdown/react/reactBasics_md'
+
+export default function WhatIsReact() {
+  return (
+    <MdLesson
+      title="React basics"
+      source={ reactBasics_md }
+      prevLesson=""
+      prevLink=""
+      nextLesson="Intermediate React"
+      nextLink="/tutorials/react/intermediate-react"
+    />
+  )
+}
